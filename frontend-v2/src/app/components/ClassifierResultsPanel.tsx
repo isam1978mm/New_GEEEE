35a5555b6d00bdbcfbd1366d81853f9786a8c98a
isam1978mm <isam78@outlook.com>
2026-08-13 09:42:11 -0400

    Restore classifier-only results UI

--- frontend-v2/src/app/components/ClassifierResultsPanel.tsx
+++ frontend-v2/src/app/components/ClassifierResultsPanel.tsx
@@ -1,15 +1,9 @@
 import { ClassifierOnlyResultsPanel } from "./ClassifierOnlyResultsPanel";
-import { Option5ResultsPanel } from "./Option5ResultsPanel";
 
 interface ClassifierResultsPanelProps {
   runId: string;
 }
 
 export function ClassifierResultsPanel({ runId }: ClassifierResultsPanelProps) {
-  return (
-    <>
-      <ClassifierOnlyResultsPanel runId={runId} />
-      <Option5ResultsPanel runId={runId} />
-    </>
-  );
+  return <ClassifierOnlyResultsPanel runId={runId} />;
 }
